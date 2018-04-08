802b97a59a1d2a942a7ee896e54a8e78352088b1
Jiazheng Li <jiazheng.li@ucdconnect.ie>
2018-04-08 01:25:37 +0000

    First Version of disk placement part Finished at Apr 8th and uploaded

---
+++ Assignment_3/disksPlacement.h
@@ -0,0 +1,8 @@
+/*PLEASE DON'T DELETE*/
+//This file is create and write by Jiazheng Li 16212162
+
+void players_placement (enum colour defiened_colour, int row, int column, disk board[SIZE][SIZE]);
+
+void Check_And_Reversi (enum colour defiened_colour, int row, int column, disk board[SIZE][SIZE]);
+
+//This file is create and written by Jiazheng Li 16212162
