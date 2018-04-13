d538698398bfe9480bfa76c4d15fb7ed391925fa
Jiazheng Li <jiazheng.li@ucdconnect.ie>
2018-04-13 11:18:05 +0000

    Same as second version, updated a line of comments

--- Assignment_3/disksPlacement.h
+++ Assignment_3/disksPlacement.h
@@ -5,5 +5,6 @@ void players_placement (enum colour defiened_colour, int row, int column, disk b
 
 void Check_And_Reversi (enum colour defiened_colour, int row, int column, disk board[SIZE][SIZE]);
 
+/*WARNING: THIS SUB-FUNCTION MUST! BE! USE! AFTER! REVERSI! FUNCTION!*/
 void Renew_Player_Points (player player1, player player2, disk board[SIZE][SIZE]);
 //This file is create and written by Jiazheng Li 16212162
