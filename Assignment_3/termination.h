8c05eb8fce1ffe96dc60584b089751ff6f2591eb
Jiazheng Li <jiazheng.li@ucdconnect.ie>
2018-04-13 11:41:56 +0000

    First version of termination

---
+++ Assignment_3/termination.h
@@ -0,0 +1,7 @@
+/*PLEASE DON'T DELETE*/
+//This file is create and written by Jiazheng Li 16212162
+
+void Termination_By_NoMoreEmptySpace (player player1, player player2 , disk board[SIZE][SIZE]);
+
+//void Termination_By_NoMoreAvaliablePosition ();
+//This file is create and written by Jiazheng Li 16212162
