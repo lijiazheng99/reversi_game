8bbecb97859bf238b19240e0464e87ea174d71f8
Jiazheng Li <jiazheng.li@ucdconnect.ie>
2018-04-13 11:43:23 +0000

    First version of save final result

---
+++ Assignment_3/SaveFinal.h
@@ -0,0 +1,7 @@
+/*PLEASE DON'T DELETE*/
+//This file is create and written by Jiazheng Li 16212162
+
+void save_final_result (player player1, player player2);
+
+
+//This file is create and written by Jiazheng Li 16212162
