425699a386f627b2f4493166276f91997be80512
Jiazheng Li <jiazheng.li@ucdconnect.ie>
2018-04-14 22:59:20 +0000

    Delete SaveFinal.h

--- Assignment_3/SaveFinal.h
+++
@@ -1,7 +0,0 @@
-/*PLEASE DON'T DELETE*/
-//This file is create and written by Jiazheng Li 16212162
-
-void save_final_result (player player1, player player2);
-
-
-//This file is create and written by Jiazheng Li 16212162
