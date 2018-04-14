6a0503b3b3737b4a13f7612c5a5bd75614e9ccab
Jiazheng Li <jiazheng.li@ucdconnect.ie>
2018-04-14 22:58:55 +0000

    Second version of terminationAndsavefinal head

---
+++ Assignment_3/terminationAndsavefinal.h
@@ -0,0 +1,13 @@
+/*PLEASE DON'T DELETE*/
+//This file is create and written by Jiazheng Li 16212162
+
+/*WARNING: THIS SUB-FUNCTION MUST! BE! USE! AFTER! REVERSI! FUNCTION!*/
+//Sub-function for termination by the condition of no more empty space
+int Termination_By_NoMoreEmptySpace (player player1, player player2 , disk board[SIZE][SIZE]);
+
+//void Termination_By_NoMoreAvaliablePosition ();
+
+//Sub-function of print the final result to a .txt file
+void save_final_result (player player1, player player2);
+
+//This file is create and written by Jiazheng Li 16212162
