d42e7335629e11934617484c9350ceff2ec301fe
Jiazheng Li <jiazheng.li@ucdconnect.ie>
2018-04-17 11:30:06 +0000

    third version of termination and save final, added check possible postion sub-function

--- Assignment_3/terminationAndsavefinal.h
+++ Assignment_3/terminationAndsavefinal.h
@@ -2,12 +2,13 @@
 //This file is create and written by Jiazheng Li 16212162
 
 /*WARNING: THIS SUB-FUNCTION MUST! BE! USE! AFTER! REVERSI! FUNCTION!*/
-//Sub-function for termination by the condition of no more empty space
-int Termination_By_NoMoreEmptySpace (player player1, player player2 , disk board[SIZE][SIZE]);
+//Sub-function for termination
+int Termination (player player1, player player2 , disk board[SIZE][SIZE]);
 
-//void Termination_By_NoMoreAvaliablePosition ();
+//Sub-function for termination sub-function to check possible position
+int Possible_position_check (enum colour defiened_colour , disk board[SIZE][SIZE]);
 
 //Sub-function of print the final result to a .txt file
-void save_final_result (player player1, player player2);
+void Save_final_result (player player1, player player2);
 
 //This file is create and written by Jiazheng Li 16212162
