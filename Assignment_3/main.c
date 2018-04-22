b5366dccad0655c45aa6830eb77cedc914ca08c0
Jiazheng Li <jiazheng.li@ucdconnect.ie>
2018-04-22 02:27:20 +0000

    Update main.c

--- Assignment_3/main.c
+++ Assignment_3/main.c
@@ -13,17 +13,41 @@
 
 int main()
 {
+    player player1 = {"",BLACK,0};
+    player player2 = {"",WHITE,0};
+    
     // Variables declaration
     
-    player player1= {"player1", BLACK,0};
-    player player2= {"player2", WHITE,0};
     disk board[SIZE][SIZE];
     
-    initializePlayers(player1, player2);
+//    initializePlayers(player1, player2);
+    unsigned long nameSize;
+    // Insert player 1
+    printf("Player 1 please insert your name:   \n");
+    fgets(player1.name, 20, stdin);
+    nameSize = strlen(player1.name);
+    player1.name[nameSize -1] = '\0';
+    
+    // Assign colours and points to player 1
+    player1.type = BLACK;
+    player1.points = 2;
+    
+    // Insert player 2
+    printf("Player 2 please insert your name:   \n");
+    fgets(player2.name, 20, stdin);
+    nameSize = strlen(player2.name);
+    player2.name[nameSize -1] = '\0';
+    
+    // Assign colours and points to player 2
+    player2.type = WHITE;
+    player2.points = 2;
+    
     
     initializeBoard(board);
     
     printBoard(board);
+    
+    
     
     int termination_sign = 0;
     
@@ -31,11 +55,37 @@ int main()
     {
         
         //For black
-        if (termination_sign == 1 || termination_sign == 0)
+        if (termination_sign != 2)
         {
             computePositions_Black(board, player1);
 
-            Renew_Player_Points (player1, player2, board);
+            int White_Counter = 0,Black_Counter = 0;
+            
+            //for loop for counting
+            for (int Row_Counter = 0; Row_Counter < 8; Row_Counter++)
+            {
+                for (int Column_Counter = 0; Column_Counter < 8; Column_Counter++)
+                {
+                    //If board is not empty, count the disk
+                    if (board[Row_Counter][Column_Counter].type != NONE)
+                    {
+                        //count white disks
+                        if(board[Row_Counter][Column_Counter].type == WHITE)
+                        {
+                            White_Counter++;
+                        }
+                        //count black disks
+                        else if(board[Row_Counter][Column_Counter].type == BLACK)
+                        {
+                            Black_Counter++;
+                        }
+                    }
+                }
+            }
+            
+            //Renew player's points after reversi
+            player1.points = Black_Counter;
+            player2.points = White_Counter;
 
             printBoard(board);
 
@@ -44,18 +94,45 @@ int main()
         termination_sign = Termination (player1, player2 , board);
         
         //For white
-        if (termination_sign == 2 || termination_sign == 0)
+        if (termination_sign != 1 && termination_sign != 3)
         {
             computePositions_White(board, player2);
             
-            Renew_Player_Points (player1, player2, board);
+            
+            int White_Counter = 0,Black_Counter = 0;
+            
+            //for loop for counting
+            for (int Row_Counter = 0; Row_Counter < 8; Row_Counter++)
+            {
+                for (int Column_Counter = 0; Column_Counter < 8; Column_Counter++)
+                {
+                    //If board is not empty, count the disk
+                    if (board[Row_Counter][Column_Counter].type != NONE)
+                    {
+                        //count white disks
+                        if(board[Row_Counter][Column_Counter].type == WHITE)
+                        {
+                            White_Counter++;
+                        }
+                        //count black disks
+                        else if(board[Row_Counter][Column_Counter].type == BLACK)
+                        {
+                            Black_Counter++;
+                        }
+                    }
+                }
+            }
+            
+            //Renew player's points after reversi
+            player1.points = Black_Counter;
+            player2.points = White_Counter;
             
             printBoard(board);
         }
         
         termination_sign = Termination (player1, player2 , board);
         
-      
+        printf("Terminate sign: %d",termination_sign);
 //        Debug
 //        printf("Debug %d",Possible_position_check(BLACK, board));
 //        printf("Debug %d",Possible_position_check(WHITE, board));
@@ -65,3 +142,4 @@ int main()
     puts("");
 
 }
+
