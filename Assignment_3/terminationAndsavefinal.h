6fb3b6bb0e772cd239aa98c69924b19ffb56174f
Ying_Ai <ying.ai@ucdconnect.ie>
2018-04-22 19:36:32 +0000

    Update terminationAndsavefinal.h

--- Assignment_3/terminationAndsavefinal.h
+++ Assignment_3/terminationAndsavefinal.h
@@ -1,5 +1,5 @@
 /*PLEASE DON'T DELETE*/
-//This file is create and written by Jiazheng Li 16212162
+//This file is create and written by YING AI 16759485
 
 /*WARNING: THIS SUB-FUNCTION MUST! BE! USE! AFTER! REVERSI! FUNCTION!*/
 //Sub-function for termination
@@ -11,4 +11,4 @@ int Possible_position_check (enum colour defiened_colour , disk board[SIZE][SIZE
 //Sub-function of print the final result to a .txt file
 void Save_final_result (player player1, player player2);
 
-//This file is create and written by Jiazheng Li 16212162
+//This file is create and written by Ying Ai 16759485
